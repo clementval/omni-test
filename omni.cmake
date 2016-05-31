b5281da85bba79b3930db53e0d0bc2a6356e2f00
Valentin Clement <clementval@gmail.com>
2016-05-31 12:30:29 +0200

    Update classpath variable

--- omni.cmake
+++ omni.cmake
@@ -12,5 +12,6 @@ set(
 )
 
 set(OMNI_F_FRONT ${OMNI_DEV_PATH}/F-FrontEnd/src/F_Front)
-set(OMNI_F_BACK  ${OMNI_DEV_PATH}/F-BackEnd/build/om-f-back.jar)
+set(OMNI_F_BACK_JARS ${OMNI_DEV_PATH}/F-BackEnd/build/om-f-back.jar:${OMNI_DEV_PATH}/XcodeML-Common/build/om-common.jar)
+set(OMNI_F_BACK xcodeml.f.util.omx2f)
 set(XMOD_DIR __xmod__)
