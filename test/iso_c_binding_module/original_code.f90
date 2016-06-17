d01c94357ee310e9453cd595c3d0a7ca60dbc452
Valentin Clement <clementval@gmail.com>
2016-06-17 08:26:27 +0200

    Add bind function and subroutine

--- test/iso_c_binding_module/original_code.f90
+++ test/iso_c_binding_module/original_code.f90
@@ -1,4 +1,13 @@
 MODULE consumer
   USE, INTRINSIC :: iso_c_binding 
 
+
+CONTAINS
+  FUNCTION SLEEP() BIND(C, NAME="sleep") 
+
+  END FUNCTION SLEEP
+
+  SUBROUTINE dummy() BIND(C, NAME="dummySleep") 
+
+  END SUBROUTINE dummy
 END MODULE consumer
