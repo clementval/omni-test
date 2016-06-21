a2a8e5abd0d49f9fbc3f7c0445951a2bc5db756b
Valentin Clement <clementval@gmail.com>
2016-06-21 10:08:56 +0200

    Add reference for protected testcase

---
+++ test/protected/reference.f90
@@ -0,0 +1,23 @@
+MODULE counter_mod
+ use xmpf_coarray_decl
+ INTEGER :: current = 0
+
+CONTAINS
+ FUNCTION next ( )
+  use xmpf_coarray_decl
+  INTEGER :: next
+
+  current = current + 1
+  next = current
+  RETURN
+ END FUNCTION next
+
+END MODULE counter_mod
+
+PROGRAM test_counter
+ use xmpf_coarray_decl
+ USE counter_mod
+
+ PRINT * , next ( )
+END PROGRAM test_counter
+
