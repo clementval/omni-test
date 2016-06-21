8f4f3a80765152102e0e5c8ffdc516fe6065f8d7
Valentin Clement <clementval@gmail.com>
2016-06-21 10:12:23 +0200

    Reference for private/public/protected test case

---
+++ test/private_public/reference.f90
@@ -0,0 +1,10 @@
+MODULE test
+ use xmpf_coarray_decl
+ REAL , PRIVATE :: a
+ REAL , PUBLIC :: b
+ REAL , PROTECTED :: p
+ REAL , PRIVATE :: c
+ REAL , PROTECTED :: w
+ REAL , PUBLIC :: v
+END MODULE test
+
