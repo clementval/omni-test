17001eec82b68fd6adbecfd8a75f9035be8a5d03
Valentin Clement <clementval@gmail.com>
2016-06-24 09:09:05 +0200

    Add reference to compare

---
+++ test/tbp1/reference.f90
@@ -0,0 +1,22 @@
+MODULE test
+ use xmpf_coarray_decl
+ TYPE :: person
+  CHARACTER ( LEN= 10 ) :: name
+  REAL :: age
+  INTEGER :: id
+ CONTAINS
+  PROCEDURE :: display_person
+  GENERIC , PUBLIC :: display => display_person
+ END TYPE person
+
+CONTAINS
+ SUBROUTINE display_person ( p )
+  use xmpf_coarray_decl
+  CLASS(person) , INTENT(IN) :: p
+  TYPE(person) :: p1
+
+  PRINT * , p % name
+ END SUBROUTINE display_person
+
+END MODULE test
+
