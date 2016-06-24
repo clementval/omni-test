b23dc56c351a9e269b9b1986b16ca0a432dc480c
Valentin Clement <clementval@gmail.com>
2016-06-24 08:51:35 +0200

    rename test case dir

---
+++ test/tbp1/original_code.f90
@@ -0,0 +1,21 @@
+MODULE test
+
+  TYPE person
+    CHARACTER(LEN=10) :: name
+    REAL              :: age
+    INTEGER           :: id
+  CONTAINS
+    PROCEDURE :: display_person
+    GENERIC, PUBLIC :: display => display_person
+  END TYPE person
+
+CONTAINS
+
+  SUBROUTINE display_person(p)
+    CLASS(person), INTENT(IN) :: p
+    TYPE(person) :: p1
+    
+    PRINT*, p%name
+  END SUBROUTINE display_person
+
+END MODULE test
