eee72a8143ebc209a3ea5c7d8028ef54ca83b5ba
Valentin Clement <clementval@gmail.com>
2016-06-28 16:13:53 +0200

    Fix fails detection

--- runAllTests.sh
+++ runAllTests.sh
@@ -6,8 +6,8 @@ FFRONT_DEV="/Users/clementval/versioning/meteoswiss/omni-compiler/F-FrontEnd"
 
 
 function runAllTests {
-  FFRONT_DIR=$1
-  FFRONT_TC=$2
+  local FFRONT_DIR=$1
+  local FFRONT_TC=$2
   local DUMMY_XML="dummy_output.xml"
   echo "RUNNING ALL TESTS ($FFRONT_TC) FOR: $FFRONT_DIR"
 
@@ -22,10 +22,10 @@ function runAllTests {
   for i in $(ls $FFRONT_DIR$FFRONT_TEST/*.f90 $FFRONT_DIR$FFRONT_TEST/*.f)
   do 
     $FFRONT_DIR$FFRONT_EXEC -o $DUMMY_XML $i 2>&1 $FRONT_TC 
-    if [[ $? != 0 ]]
+    if [ $? -ne 0 ] 
     then
       NAKED_TC=${i#$FFRONT_DIR}
-      echo "TC failed $NAKED_TC" >> $FFRONT_TC 
+      echo "$NAKED_TC" >> $FFRONT_TC 
       FAILED_TESTS=$(($FAILED_TESTS+1))
     else
       SUCCESSTESTS=$(($SUCCESSTESTS+1))
@@ -46,7 +46,7 @@ DEV_OUTPUT="dev.output"
 
 runAllTests $FFRONT_REFERENCE $REF_OUTPUT
 
-runAllTests $FFRONT_DEV $DEV_OUTPUT
-
-diff $REF_OUTPUT $DEV_OUTPUT
+#runAllTests $FFRONT_DEV $DEV_OUTPUT
+#
+#diff $REF_OUTPUT $DEV_OUTPUT
 
