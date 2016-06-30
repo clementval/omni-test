d63c1f035fd2b14629ed83b905c2da9b7c1c6dba
Valentin Clement <clementval@gmail.com>
2016-06-30 11:34:28 +0200

    Fix example to compile with gfortran

--- test/dataref1/main.f90
+++ test/dataref1/main.f90
@@ -2,6 +2,11 @@ PROGRAM main
   USE shape_mod
   
   type(shape) :: shp        ! declare an instance of shape
+  type(square) :: sq        ! declare an instance of shape
   logical filled
-  filled = shp%isFilled()
+  !filled = shp%isFilled()
+  !CALL shp%isFilled()
+  sq%x = 4.0
+  sq%y = 4.0
+  CALL sq%shape%print_coord()
 END PROGRAM
--- test/dataref1/original_code.f90
+++ test/dataref1/original_code.f90
@@ -6,10 +6,21 @@ module shape_mod
     integer :: y
   contains
     procedure :: isFilled
+    procedure :: print_coord
   end type shape
+
+  type, extends(shape) :: square
+  end type
+
 contains
   logical function isFilled(this)
     class(shape) :: this
     isFilled = this%filled
   end function
+
+  SUBROUTINE print_coord(this)
+    class(shape) :: this
+    print*,this%x
+    print*,this%y
+  END SUBROUTINE
 end module
