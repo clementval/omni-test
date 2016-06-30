353db4510bc7b60295cb980a3d84cb5511bbd969
Valentin Clement <clementval@gmail.com>
2016-06-30 11:44:29 +0200

    Add ref for module

---
+++ test/dataref1/reference.f90
@@ -0,0 +1,32 @@
+MODULE shape_mod
+ use xmpf_coarray_decl
+ TYPE :: shape
+  INTEGER :: color
+  LOGICAL :: filled
+  INTEGER :: x
+  INTEGER :: y
+  PROCEDURE :: isfilled
+  PROCEDURE :: print_coord
+ END TYPE shape
+ TYPE :: square
+ END TYPE square
+
+CONTAINS
+ FUNCTION isfilled ( this )
+  use xmpf_coarray_decl
+  LOGICAL :: isfilled
+  CLASS(shape) :: this
+
+  isfilled = this % filled
+ END FUNCTION isfilled
+
+ SUBROUTINE print_coord ( this )
+  use xmpf_coarray_decl
+  CLASS(shape) :: this
+
+  PRINT * , this % x
+  PRINT * , this % y
+ END SUBROUTINE print_coord
+
+END MODULE shape_mod
+
