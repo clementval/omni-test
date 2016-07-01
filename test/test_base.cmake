41defea81ef435c432213b340e47632821758d0a
Valentin Clement <clementval@gmail.com>
2016-07-01 10:12:41 +0200

    Homogeneous naming

--- test/test_base.cmake
+++ test/test_base.cmake
@@ -4,31 +4,31 @@
 # OMNI Compiler test case base set up
 
 # Set up input/output filenames
-set(ORIGINAL_FILE  ${CMAKE_CURRENT_SOURCE_DIR}/original_code.f90)
-set(MAIN_FILE      ${CMAKE_CURRENT_SOURCE_DIR}/main.f90)
-set(XCODEML_FILE   ${CMAKE_CURRENT_SOURCE_DIR}/transformed_code.xml)
-set(MAIN_XCML_FILE ${CMAKE_CURRENT_SOURCE_DIR}/main_transformed.xml)
-set(OUTPUT_FILE    ${CMAKE_CURRENT_SOURCE_DIR}/transformed_code.f90)
-set(MAIN_OUTPUT    ${CMAKE_CURRENT_SOURCE_DIR}/transformed_main.f90)
-set(REFERENCE_FILE ${CMAKE_CURRENT_SOURCE_DIR}/reference.f90)
-set(REFERENCE_MAIN ${CMAKE_CURRENT_SOURCE_DIR}/reference_main.f90)
+set(ORIGINAL_FILE           ${CMAKE_CURRENT_SOURCE_DIR}/original_code.f90)
+set(MAIN_FILE               ${CMAKE_CURRENT_SOURCE_DIR}/main.f90)
+set(XCODEML_ORIGINAL_FILE   ${CMAKE_CURRENT_SOURCE_DIR}/transformed_code.xml)
+set(XCODEML_MAIN_FILE       ${CMAKE_CURRENT_SOURCE_DIR}/transformed_main.xml)
+set(OUTPUT_ORIGINAL_FILE    ${CMAKE_CURRENT_SOURCE_DIR}/transformed_code.f90)
+set(OUTPUT_MAIN_FILE        ${CMAKE_CURRENT_SOURCE_DIR}/transformed_main.f90)
+set(REFERENCE_ORIGINAL_FILE ${CMAKE_CURRENT_SOURCE_DIR}/reference.f90)
+set(REFERENCE_MAIN_FILE     ${CMAKE_CURRENT_SOURCE_DIR}/reference_main.f90)
 
 
 if(${HAS_MAIN})
 # Transform Fortran to XcodeML to Fortran
   add_custom_target(transform-${TEST_NAME}
-    ${OMNI_F_FRONT} -o ${XCODEML_FILE} ${ORIGINAL_FILE}
-    COMMAND ${OMNI_F_FRONT} -o ${MAIN_XCML_FILE} ${MAIN_FILE}
-    COMMAND ${Java_JAVA_EXECUTABLE} -cp ${OMNI_F_BACK_JARS} ${OMNI_F_BACK} -l -w 80 -o ${OUTPUT_FILE} ${XCODEML_FILE}
-    COMMAND ${Java_JAVA_EXECUTABLE} -cp ${OMNI_F_BACK_JARS} ${OMNI_F_BACK} -l -w 80 -o ${MAIN_OUTPUT} ${MAIN_XCML_FILE}
+    ${OMNI_F_FRONT} -o ${XCODEML_ORIGINAL_FILE} ${ORIGINAL_FILE}
+    COMMAND ${OMNI_F_FRONT} -o ${XCODEML_MAIN_FILE} ${MAIN_FILE}
+    COMMAND ${Java_JAVA_EXECUTABLE} -cp ${OMNI_F_BACK_JARS} ${OMNI_F_BACK} -l -w 80 -o ${OUTPUT_ORIGINAL_FILE} ${XCODEML_ORIGINAL_FILE}
+    COMMAND ${Java_JAVA_EXECUTABLE} -cp ${OMNI_F_BACK_JARS} ${OMNI_F_BACK} -l -w 80 -o ${OUTPUT_MAIN_FILE} ${XCODEML_MAIN_FILE}
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
     DEPENDS ${ORIGINAL_FILE} ${MAIN_FILE}
     COMMENT "Process XcodeML to Fortran with OMNI Fortran back-end (${OMNI_F_BACK})"
   )
 else()
   add_custom_target(transform-${TEST_NAME}
-    ${OMNI_F_FRONT} -o ${XCODEML_FILE} ${ORIGINAL_FILE}
-    COMMAND ${Java_JAVA_EXECUTABLE} -cp ${OMNI_F_BACK_JARS} ${OMNI_F_BACK} -l -w 80 -o ${OUTPUT_FILE} ${XCODEML_FILE}
+    ${OMNI_F_FRONT} -o ${XCODEML_ORIGINAL_FILE} ${ORIGINAL_FILE}
+    COMMAND ${Java_JAVA_EXECUTABLE} -cp ${OMNI_F_BACK_JARS} ${OMNI_F_BACK} -l -w 80 -o ${OUTPUT_ORIGINAL_FILE} ${XCODEML_ORIGINAL_FILE}
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
     DEPENDS ${ORIGINAL_FILE}
     COMMENT "Process XcodeML to Fortran with OMNI Fortran back-end (${OMNI_F_BACK})"
@@ -38,7 +38,7 @@ endif()
 # Target to clean the generated file
 add_custom_target(
   clean-${TEST_NAME}
-  COMMAND rm -f ${OUTPUT_FILE} ${OUTPUT_MAIN} ${MAIN_XCML_FILE} ${XCODEML_FILE} *.xmod
+  COMMAND rm -f ${OUTPUT_ORIGINAL_FILE} ${OUTPUT_MAIN} ${XCODEML_MAIN_FILE} ${XCODEML_ORIGINAL_FILE} *.xmod
 )
 
 # Add local targets to the global targets
@@ -48,9 +48,9 @@ add_dependencies(${CLEAN_TEST_TARGET} clean-${TEST_NAME})
 # Add test targets
 add_test(NAME omni-transform-${TEST_NAME} COMMAND "${CMAKE_COMMAND}"  --build ${CMAKE_BINARY_DIR} --target transform-${TEST_NAME})
 if(${HAS_MAIN})
-  add_test(NAME omni-compare-${TEST_NAME} COMMAND diff ${OUTPUT_FILE} ${REFERENCE_FILE})
-  add_test(NAME omni-compare-${TEST_NAME}-main COMMAND diff ${MAIN_OUTPUT} ${REFERENCE_MAIN})
+  add_test(NAME omni-compare-${TEST_NAME} COMMAND diff ${OUTPUT_ORIGINAL_FILE} ${REFERENCE_ORIGINAL_FILE})
+  add_test(NAME omni-compare-${TEST_NAME}-main COMMAND diff ${OUTPUT_MAIN_FILE} ${REFERENCE_MAIN_FILE})
 else()
-  add_test(NAME omni-compare-${TEST_NAME} COMMAND diff ${OUTPUT_FILE} ${REFERENCE_FILE})
+  add_test(NAME omni-compare-${TEST_NAME} COMMAND diff ${OUTPUT_ORIGINAL_FILE} ${REFERENCE_ORIGINAL_FILE})
 endif()
 set_tests_properties(omni-compare-${TEST_NAME} PROPERTIES DEPENDS omni-transform-${TEST_NAME})
