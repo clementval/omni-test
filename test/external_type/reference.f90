512f0b72ca860d1ad5069ada89aafcb8134f0008
Valentin Clement <clementval@gmail.com>
2016-07-21 08:26:32 +0200

    Add refences

---
+++ test/external_type/reference.f90
@@ -0,0 +1,8 @@
+MODULE testmod
+ use xmpf_coarray_decl
+ TYPE :: mytype
+  REAL :: r1
+  REAL :: r2
+ END TYPE mytype
+END MODULE testmod
+
---
+++ test/external_type/reference_main.f90
@@ -0,0 +1,11 @@
+PROGRAM main
+ use xmpf_coarray_decl
+ USE testmod
+ CLASS(mytype) :: this
+
+ this % r1 = 0.1
+ this % r2 = 0.2
+ PRINT * , this % r1
+ PRINT * , this % r2
+END PROGRAM main
+
