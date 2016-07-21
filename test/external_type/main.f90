948e0c49b6e14b9fedc4349cfe74440135e7f31b
Valentin Clement <clementval@gmail.com>
2016-07-21 09:59:17 +0200

    Update test case with polymorphic member access

--- test/external_type/main.f90
+++ test/external_type/main.f90
@@ -1,13 +1,25 @@
-
 PROGRAM main
   USE testmod
 
-  CLASS(mytype) :: this
+  CLASS(mytype), ALLOCATABLE :: this
+  CLASS(mytype2), ALLOCATABLE :: this2
+
+  ALLOCATE(this)
+  ALLOCATE(this2)
 
   this%r1 = 0.1
   this%r2 = 0.2
 
+  this2%r1 = 0.1
+  this2%r2 = 0.2
+
+  CALL test(this)
+  CALL test(this2)
+
   PRINT*, this%r1
   PRINT*, this%r2
+
+  DEALLOCATE(this)
+  DEALLOCATE(this2)
 
 END PROGRAM main
--- test/external_type/original_code.f90
+++ test/external_type/original_code.f90
@@ -5,5 +5,22 @@ MODULE testmod
     REAL :: r2
   END TYPE
 
+  TYPE, EXTENDS(mytype) :: mytype2
+    REAL :: r3
+  END TYPE
+
+CONTAINS
+
+SUBROUTINE test(t) 
+  CLASS(mytype) :: t 
+ 
+  SELECT TYPE (t) 
+    CLASS IS (mytype) 
+      t%r1 = t%r2 
+    CLASS IS (mytype2) 
+      t%r3 = t%r2     
+  END SELECT 
+END SUBROUTINE
+
 
 END MODULE testmod
