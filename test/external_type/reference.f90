85c70fcd598c1ab9150110306524f96681a6d1ce
Valentin Clement <clementval@gmail.com>
2016-07-21 11:47:39 +0200

    Update module ref

--- test/external_type/reference.f90
+++ test/external_type/reference.f90
@@ -4,5 +4,22 @@ MODULE testmod
   REAL :: r1
   REAL :: r2
  END TYPE mytype
+ TYPE , EXTENDS ( mytype ) :: mytype2
+  REAL :: r3
+ END TYPE mytype2
+
+CONTAINS
+ SUBROUTINE test ( t )
+  use xmpf_coarray_decl
+  CLASS(mytype) :: t
+
+  SELECT TYPE ( t )
+  CLASS IS ( mytype )
+   t % r1 = t % r2
+  CLASS IS ( mytype2 )
+   t % r3 = t % r2
+  END SELECT
+ END SUBROUTINE test
+
 END MODULE testmod
 
