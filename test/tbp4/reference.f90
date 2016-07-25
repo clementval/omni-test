030ba0d5d4c07d6fe131f43620bc178b347acfe6
Valentin Clement <clementval@gmail.com>
2016-07-25 14:52:16 +0200

    Add reference code

---
+++ test/tbp4/reference.f90
@@ -0,0 +1,21 @@
+MODULE shape
+ use xmpf_coarray_decl
+ TYPE :: point
+  REAL :: x
+  REAL :: y
+  PROCEDURE , PASS :: length
+ END TYPE point
+
+CONTAINS
+ FUNCTION length ( a , b ) RESULT(result_value)
+  use xmpf_coarray_decl
+  REAL :: result_value
+  CLASS(point) , INTENT(IN) :: a
+  CLASS(point) , INTENT(IN) :: b
+
+  result_value = sqrt ( ( a % x - b % x ) ** ( 2 ) + ( a % y - b % y ) ** ( 2&
+   ) )
+ END FUNCTION length
+
+END MODULE shape
+
