166bf110d981785196a9afcb137d94ccd7b7c2be
Valentin Clement <clementval@gmail.com>
2016-09-10 15:22:24 +0200

    Add references

---
+++ test/class_array/reference.f90
@@ -0,0 +1,8 @@
+MODULE shape_mod
+ use xmpf_coarray_decl
+ TYPE :: shape
+  INTEGER :: x
+  INTEGER :: y
+ END TYPE shape
+END MODULE shape_mod
+
---
+++ test/class_array/reference_main.f90
@@ -0,0 +1,7 @@
+PROGRAM main
+ use xmpf_coarray_decl
+ USE shape_mod
+ CLASS(shape) :: shp ( 1 : 10 )
+
+END PROGRAM main
+
